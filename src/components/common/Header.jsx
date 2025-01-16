299cb65943de176e9b51ca714aeb6f36a8529795
HUZEFAGIT <huzefashaikh551@gmail.com>
2025-01-16 12:30:50 +0530

    new changes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -8,7 +8,11 @@ const Header = () => {
         <div className="flex justify-between items-center bg-gray-900 text-white p-4 mb-6">
             <div className="flex items-center space-x-4">
                 <img src={RLogo} width={50}></img>
-                <div className="text-xl font-semibold">RADIAANT</div>
+                <div className="flex flex-col text-left">
+                    <div className="text-xl font-semibold">RADIAANT</div>
+                    <div className="text-sm font-semibold">Captive India Pvt. Ltd.</div>
+                </div>
+
             </div>
             <div className="flex items-center space-x-4">
                 <div className="text-sm">
